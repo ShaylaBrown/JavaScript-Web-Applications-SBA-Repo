430f3eed35f4ddd43a2ba0ee5309bacd298c1db9
Shayla Brown <shaylabrown227@gmail.com>
2024-05-13 15:39:54 -0500

    added a url using the fetch method, and added my api key and chained the .then() method to the fetch funtion

--- script.mjs
+++ script.mjs
@@ -1,2 +1,26 @@
+const url = `https://api.thecatapi.com/v1/breeds`;
+const api_key = "x-api-key"
+
+
+
 const authorContainer = document.getElementById('cat-container');
 const loadMoreBtn = document.getElementById('load-more-btn');
+
+let startingIndex = 0;
+let endingIndex = 8;
+let catDataArr = [];
+
+fetch('https://api.thecatapi.com/v1/images/search?limit=10.json')
+.then((res) => res.json())
+  .then((data) => {
+    console.log(data);   
+  })
+  .catch((err) => {
+    console.error(`There was an error: ${err}`);
+  });
+
+  const displayCats = (cats) => {
+    cats.forEach(({ cats, image, url, breed }, index) => {
+       
+  });
+}; 
