cd6a347271f58bdd37614000044a8c061676f43d
Shayla Brown <shaylabrown227@gmail.com>
2024-05-15 15:03:24 -0500

    added a .catch() for error handeling

--- script.mjs
+++ script.mjs
@@ -12,8 +12,8 @@ fetch('https://api.thecatapi.com/v1/breeds?limit=20')
     catDataArr = data;
     displayCats(catDataArr.slice(startingIndex, endingIndex)); 
   })
-  .catch((err) => {
-    console.error(`There was an error: ${err}`);
+    .catch((err) => {
+      catContainer.innerHTML = '<p class="error-msg">There was an error loading the cats</p>';
   });
   const fetchMoreCats = () => {
     startingIndex += 8;
@@ -30,12 +30,16 @@ fetch('https://api.thecatapi.com/v1/breeds?limit=20')
     cats.forEach(({ cats, image, url, breed }, index) => {
       catContainer.innerHTML += `
       <div id="${index}" class="user-card">
+      
       <h2 class="author-name">${cats}</h2>
       <img class="user-img" src="${image}" alt="${cats} avatar" />
-      <p class="bio">${breed}</p>
+      <div class="purple-divider"></div>
+      <p class="breed">${breed}</p>
       <a class="author-link" href="${url}" target="_blank">${cats}'s author page</a>
     </div>
   `;
   });
 }; 
 loadMoreBtn.addEventListener("click", fetchMoreCats);
+
+// {/* <p class="breed">${breed.length > 20 ? breed.slice(0, 20) + "..." : breed}</p> */}
