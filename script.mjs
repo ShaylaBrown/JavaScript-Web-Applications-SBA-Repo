aec76b1a347c746ec5bcca9be079fdd491c01a9c
ShaylaBrown <164940252+ShaylaBrown@users.noreply.github.com>
2024-05-17 17:05:43 -0500

    added to inner HTML, interpolated an api key, accesed the data from the object element

--- script.mjs
+++ script.mjs
@@ -5,16 +5,24 @@ const loadMoreBtn = document.getElementById('load-more-btn');
 let startingIndex = 0;
 let endingIndex = 8;
 let catDataArr = [];
+let api_key = "live_h1zEilC3htrQwMJ1soufLQAeVIBQ5giPo5fUIOtTNXwrmznomGAjDH5LMM9FGKzf"
 
-fetch('https://api.thecatapi.com/v1/breeds?limit=20')
-.then((res) => res.json())
-  .then((data) => {
-    catDataArr = data;
-    displayCats(catDataArr.slice(startingIndex, endingIndex)); 
-  })
-    .catch((err) => {
-      catContainer.innerHTML = '<p class="error-msg">There was an error loading the cats</p>';
-  });
+fetch(`https://api.thecatapi.com/v1/breeds?limit=20?api_key=${api_key}`)
+.then(async(res) => {
+  const value = await res.json()
+  console.log(value);
+  // console.log(res.json);
+  // return(res.json);
+displayCats(value); 
+} )
+  // .then((data) => {
+  //   console.log(data);
+  //   catDataArr = data;
+    
+  // })
+  //   .catch((err) => {
+  //     catContainer.innerHTML = '<p class="error-msg">There was an error loading the cats</p>';
+  // // });
   const fetchMoreCats = () => {
     startingIndex += 8;
     endingIndex += 8;
@@ -28,19 +36,30 @@ fetch('https://api.thecatapi.com/v1/breeds?limit=20')
   };
 
   const displayCats = (cats) => {
-    cats.forEach(({ cats, image, url, breed }, index) => {
+    cats.forEach((element, index) => {
+      console.log(element);
       catContainer.innerHTML += `
       <div id="${index}" class="user-card">
       
-      <h2 class="author-name">${cats}</h2>
-      <img class="user-img" src="${image}" alt="${cats} avatar" />
-      <div class="purple-divider"></div>
-      <p class="breed">${breed}</p>
-      <a class="author-link" href="${url}" target="_blank">${cats}'s author page</a>
+      <h2 class="cat-name">${element.name}</h2>
+      <div class="gold-divider"></div>
+      <p class="description">${element.description}</p>
+      <a class="profile-link" href="${element.vcahospitals_url}" target="_blank">${element.name}'s profile page</a>
     </div>
-  `;
-  });
+  `}); 
+  console.log(cats);
 }; 
+
 loadMoreBtn.addEventListener("click", fetchMoreCats);
 
-// {/* <p class="breed">${breed.length > 20 ? breed.slice(0, 20) + "..." : breed}</p> */}
+
+//here I tried to make all of the descriptions end with an elipsis after a certain point but it did not work.//
+// {/* <p class="description">${description.length > 20 ? description.slice(0, 20) + "..." : description}</p> */}
+
+async function catdata() {
+const catvariable = await fetch('https://api.thecatapi.com/v1/breeds?limit=20');
+const anothervariable = await catvariable.json();
+console.log(anothervariable);
+}
+
+catdata()
